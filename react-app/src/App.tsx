e50074aeb30456190abf849de78986c866f56d2b
kevinandrep <62022999+kevinandrep@users.noreply.github.com>
2025-05-03 22:06:13 -0500

    ejercicio boton propio

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List ";
 
 function App() {
   const list = ["Audra", "Uqi", "hasdr"];
+  const [status, setStatus] = useState(true);
 
   const handleSelect = (elemento: string) => {
     console.log(elemento);
+  };
+
+  const handleClick = () => {
+    setStatus(!status);
   };
   return (
     <Card>
       <CardBody cardTitle="Titulo" cardText="Este es el texto"></CardBody>
       <List data={list} onSelect={handleSelect}></List>
+
+      <button
+        onClick={handleClick}
+        type="button"
+        className={`${status ? "btn btn-primary" : "btn btn-secondary"}`}
+      >
+        {status ? "hola mundo" : "Cargando"}
+      </button>
     </Card>
   );
 }
